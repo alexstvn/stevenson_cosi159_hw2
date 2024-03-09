7348468f52c158063c4eae11aca8c4f19c318f89
Alex Stevenson <121518228+alexstvn@users.noreply.github.com>
2024-03-08 19:53:37 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# stevenson_cosi159_hw2
+# Assignment 02
+
+## Setting Virtual Environment.
+1. Using Kaggle, import the notebook by creating a notebook then going the **File > Import Notebook**.
+2. To import datasets and other code, in the notebook, go to **File > Upload input**. Input the LFW datasets and other code files other than the notebook. Label the input folder as “assignment2input”.
+3. To import the image folder, upload a zip folder of the lfw image data. Ensure that in the zip folder, it contains no additional sub folders.
+
+You can also find the notebook online in Kaggle [here](https://www.kaggle.com/code/alexstvn/assignment2).
---
+++ utils.py
@@ -0,0 +1,35 @@
+# @Author  : Peizhao Li
+# @Contact : peizhaoli05@gmail.com
+
+import random
+import numpy as np
+
+import torch
+
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    return
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
